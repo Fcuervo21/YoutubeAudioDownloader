96e9898dbf0933c7e11e63e0c020ed0774269fcc
Fernando Cuervo <fcuervo@RavenMac.local>
2022-05-18 23:34:18 -0500

    Final version

--- finalDownloader.py
+++ finalDownloader.py
@@ -2,27 +2,32 @@ from pytube import YouTube
 import os
 
 # url input from user
-entry_url = input("Enter the URL of the video you want to download: ")
-yt = YouTube(entry_url)
-  
-# extract only audio
-#video = yt.streams.filter(only_audio=True).first()
-video = yt.streams.get_audio_only()
-  
-# check for destination to save file
-print("Enter the destination (leave blank for current directory)")
-destination = str(input(">> ")) or '.'
-  
-# download the file
-out_file = video.download(output_path=destination)
-  
-# save the file
-base, ext = os.path.splitext(out_file)
-new_file = base + '.mp4'
-os.rename(out_file, new_file)
-  
-# result of success
-print(yt.title + " has been successfully downloaded.")
+def downloadSong(link):
+    #entry_url = input("Enter the URL of the video you want to download: ")
+    yt = YouTube(link)
+    
+    # extract only audio
+    #video = yt.streams.filter(only_audio=True).first()
+    video = yt.streams.get_audio_only()
+    
+    # # check for destination to save file
+    # print("Enter the destination (leave blank for current directory)")
+    # destination = str(input(">> ")) or '.'
+    
+    # download the file
+    out_file = video.download(output_path='.')
+    
+    # save the file
+    base, ext = os.path.splitext(out_file)
+    new_file = base + '.mp4'
+    os.rename(out_file, new_file)
+    
+    # result of success
+    print(yt.title + " has been successfully downloaded.")
 
-# if __name__ == "__main__":
-#     pass
+if __name__ == "__main__":
+    my_file = open("YOUR TXT FILE PATH", "r")
+    content = my_file.read().split("\n")
+    print("The songs you placed in the file are about to be downloaded...\n")
+    for i in content:
+        downloadSong(i)
